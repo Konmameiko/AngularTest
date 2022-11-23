3f6a343d2e77df970309744bc3e9840cd5abd243
konmameiko <a13224010118@163.com>
2022-11-23 20:51:42 +0800

    MODIFY:修改项目常用类型为SCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+/*
+ * @Description: 
+ * @Author: KonmaMeiko
+ * @Date: 2022-11-22 23:47:51
+ * @lastEditTime: Do not edit
+ * @LastEditors: KonmaMeiko
+ */
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
